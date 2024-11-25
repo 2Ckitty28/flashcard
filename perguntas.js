591ef6745903ce999cc5ecb132383d781585b9c4
2Ckitty28 <00001111054885sp@al.educacao.sp.gov.br>
2024-11-25 08:41:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'geografia',
+    'Qual a capital da grecia?',
+    'Atenas'
+)
+/* Guilherme.Fidelis.2c.20/10/2024
+*/
+criaCartao(
+    'volei',
+    'quantos jogadores entram em quadra?',
+    '6 jogadores '
+)
+
+criaCartao(
+    'lingua inglesa',
+    'como se diz "amigo" em ingles?',
+    'friend'
+)
+
+criaCartao(
+    'lingua portuguesa',
+    'qual o plural de pão?',
+    'pães'
+)
